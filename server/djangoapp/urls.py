ee6cf49ad3500fed0e4e0398857045469046ccf2
alchemistcohen <alancohen7@gmail.com>
2025-07-09 14:01:01 -0400

    Actualizacion

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,14 +4,17 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
+
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-
+    # path for registration
+     
     # path for login
     path(route='login', view=views.login_user, name='login'),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    # path for dealer reviews view
+    # path for logout
+      path(route='logout', view=views.logout_request, name='logout'),
+    # path for getting the list of cars
+    
 
     # path for add a review view
 
